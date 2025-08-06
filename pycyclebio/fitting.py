531e68412598b6bd75314b0525836992f18c0a5a
AlexBennett <a.rw.bennett@gmail.com>
2025-08-06 15:23:19 +0200

    Code clean up

--- pycyclebio/fitting.py
+++ pycyclebio/fitting.py
@@ -514,7 +514,7 @@ def fit_repro(df_in):
     return df_out.sort_values(by='p-val').sort_values(by='BH-padj')
 
 
-def pcbplot(data, res, molecule, period=24, colour=None, path = None):
+def pcbplot(data, res, molecule, period=24, colour=None, path=None):
 
     data = data.set_index(data.columns[0])
     res = res.set_index(res.columns[0])
@@ -550,7 +550,7 @@ def pcbplot(data, res, molecule, period=24, colour=None, path = None):
     plt.scatter(x=timepoints_plot, y=measurements.transpose(), color="#000000")
     plt.plot(t_plot, eq, color=wavecolour, linewidth=3)
     plt.xticks(np.arange(0, np.max(timepoints_plot), 6))
-    if path != None:
+    if path:
         plt.savefig(path)
     plt.show()
 
@@ -595,4 +595,3 @@ def get_regs(data, res, target, regs, period=24):
 
     else:
         return rhythmic_regs.sort_values(by='BH-padj')
-
